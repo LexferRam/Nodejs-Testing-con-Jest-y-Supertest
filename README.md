77c88e79668481f5ddab6c166f94cde1a7e24351
Lexfer <lexferramirez@gmail.com>
2021-10-25 21:53:48 -0400

    create first test within a describe  block

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Nodejs-Testing-con-Jest-y-Supertest
+
+ Apuntest
+
+import request from 'supertest';//similar a fetch del lado del cliente
+
+# para ejecutar jest desde consola
+
+# LINUX
+
+//export NODE_OPTIONS=--experimental-vm-modules && npx jest
+
+# WINDOWS
+
+//set NODE_OPTIONS=--experimental-vm-modules && npx jest
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,1 +1,12 @@
-import jest from 'jest';
+import app from '../src/app';
+import request from 'supertest';
+
+describe('GET /task', () => {
+
+    test('should respond with a 200 status code',async () => {
+        const response = await request(app).get('/tasks').send()
+        expect(response.status).toBe(200)
+    });
+
+})
+
